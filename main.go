d5ca299f1ed4deb23d5c6dfeb5a14df324f3457f
EironnESP <aaronlozanocastillo@gmail.com>
2025-05-06 18:15:27 +0200

    interfaz inicial creada

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/kbinani/screenshot"
 	_ "github.com/mattn/go-sqlite3"
@@ -21,6 +24,11 @@ func main() {
 
 	//VARIABLES GLOBALES
 	home, _ := os.UserHomeDir()
+	semaforoSize := fyne.NewSize(28, 100)
+	modoEdicion := false
+	inicializado := false
+	numDirecciones := 0
+	botones := make([]*widget.Button, 0)
 
 	//INICIALIZACION BBDD, ¿PREGUNTAR O COMPROBAR?
 	err := inicializarDB(home)
@@ -44,27 +52,110 @@ func main() {
 	}
 	w.SetIcon(icono)
 
-	//CARGAR FONDO
+	//CARGAR IMAGENES
+	semVerde := canvas.NewImageFromFile("images/semaforos/semVerde.png")
+	semVerde.FillMode = canvas.ImageFillOriginal
+	semVerde.Resize(semaforoSize)
+
+	semAmbar := canvas.NewImageFromFile("images/semaforos/semAmbar.png")
+	semAmbar.FillMode = canvas.ImageFillOriginal
+	semAmbar.Resize(semaforoSize)
+
+	semRojo := canvas.NewImageFromFile("images/semaforos/semRojo.png")
+	semRojo.FillMode = canvas.ImageFillOriginal
+	semRojo.Resize(semaforoSize)
+
+	semApagado := canvas.NewImageFromFile("images/semaforos/semApagado.png")
+	semApagado.FillMode = canvas.ImageFillOriginal
+	semApagado.Resize(semaforoSize)
+
+	//FONDO
 	image := canvas.NewImageFromFile("images/cruces/cruce_vacio.png")
 	image.FillMode = canvas.ImageFillOriginal
-	w.SetContent(image)
+
+	layoutBotonesEditar := container.NewWithoutLayout()
+	fondo := container.NewStack(image, layoutBotonesEditar)
+
+	semTest := semAmbar
+	layoutBotonesEditar.Add(semTest)
+	semTest.Move(fyne.NewPos(500, 500))
+	fyne.DoAndWait(func() { ambar(semAmbar, semApagado, semTest, layoutBotonesEditar) })
+
+	//BARRAS DE HERRAMIENTAS
+	barraHerramientasEdicion := widget.NewToolbar(
+		widget.NewToolbarAction(theme.DocumentSaveIcon(), func() {
+			//HACER EN ACCESO A DATOS
+			fmt.Println("guardar diseño")
+		}),
+		widget.NewToolbarSeparator(),
+		widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
+			if modoEdicion {
+				modoEdicion = false
+				colocarBotones(layoutBotonesEditar, botones, numDirecciones, inicializado, modoEdicion)
+			} else {
+				modoEdicion = true
+				colocarBotones(layoutBotonesEditar, botones, numDirecciones, inicializado, modoEdicion)
+			}
+		}),
+		widget.NewToolbarAction(theme.VisibilityIcon(), func() {
+			fmt.Println("cambiar orientacion")
+		}),
+		widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.HelpIcon(), func() {
+			//DISTRIBUCION
+			fmt.Println("ayuda")
+		}),
+	)
+
+	barraHerramientasEjecucion := widget.NewToolbar(
+		widget.NewToolbarAction(theme.MediaPlayIcon(), func() {}),
+		widget.NewToolbarAction(theme.MediaPauseIcon(), func() {}),
+		widget.NewToolbarAction(theme.MediaReplayIcon(), func() {}),
+	)
+
+	contentEdicion := container.NewBorder(barraHerramientasEdicion, nil, nil, nil, fondo)
+	contentEjecucion := container.NewBorder(barraHerramientasEjecucion, nil, nil, nil, fondo)
+
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Edición", contentEdicion),
+		container.NewTabItem("Ejecución", contentEjecucion),
+	)
+
+	w.SetContent(tabs)
 	w.Show()
 
-	//PANTALLA NUEVO DISEÑO O EXISTENTE
-	w1 := a.NewWindow("Abrir diseño")
-	w1.Resize(fyne.NewSize(200, 80))
-	w1.CenterOnScreen()
-	w1.SetIcon(icono)
-
+	//DIALOG NUEVO DISEÑO O EXISTENTE
 	var c *fyne.Container
+	var d *dialog.CustomDialog
+	var dDir *dialog.CustomDialog
 
 	botonNuevo := widget.NewButton("Nuevo diseño", func() {
-		defer w1.Close()
 		fmt.Println("nuevo")
+
+		boton3 := widget.NewButton("3 direcciones", func() {
+			numDirecciones = 3
+			image = canvas.NewImageFromFile("images/cruces/cruce3_vacio.png")
+			image.FillMode = canvas.ImageFillOriginal
+			fondo.Objects[0] = image
+			fondo.Refresh()
+			colocarBotones(layoutBotonesEditar, botones, numDirecciones, inicializado, modoEdicion)
+			dDir.Hide()
+		})
+		boton4 := widget.NewButton("4 direcciones", func() {
+			numDirecciones = 4
+			colocarBotones(layoutBotonesEditar, botones, numDirecciones, inicializado, modoEdicion)
+			dDir.Hide()
+		})
+
+		c = container.New(layout.NewVBoxLayout(), boton3, boton4)
+
+		dDir = dialog.NewCustomWithoutButtons("Tipo de intersección", c, w)
+		d.Hide()
+		dDir.Show()
 	})
 
 	//HACER EN ACCESO A DATOS
-	botonAbrir := widget.NewButton("Abrir diseño", func() {
+	botonAbrir := widget.NewButton("Abrir diseño guardado", func() {
 		fmt.Println("abrir")
 
 		db, err := sql.Open("sqlite3", home+"/.intersecciones/test.db")
@@ -88,8 +179,8 @@ func main() {
 		for rows.Next() {
 			err = rows.Scan(&nombre, &num)
 			if num == 0 {
-				mostrarError("No hay diseños guardados", a)
-				w1.Close()
+				mostrarError("No hay diseños guardados", a, w)
+				d.Hide()
 				return
 			}
 
@@ -100,7 +191,7 @@ func main() {
 
 			fmt.Println(nombre)
 			c.Objects = append(c.Objects, widget.NewButton(nombre, func() {
-				defer w1.Close()
+				d.Hide()
 				fmt.Println("abrir diseño: " + nombre)
 
 			}))
@@ -108,13 +199,15 @@ func main() {
 	})
 
 	c = container.New(layout.NewVBoxLayout(), botonNuevo, botonAbrir)
-	w1.SetContent(c)
-	w1.Show()
 
+	d = dialog.NewCustomWithoutButtons("Abrir diseño", c, w)
+	d.Show()
+	inicializado = true
 	a.Run()
 }
 
-func mostrarError(e string, a fyne.App) {
+// VENTANA DE ERROR
+func mostrarError(e string, a fyne.App, w ...fyne.Window) {
 	wError := a.NewWindow("Error")
 	wError.CenterOnScreen()
 	icono, err := fyne.LoadResourceFromPath("images/icono.png")
@@ -131,6 +224,9 @@ func mostrarError(e string, a fyne.App) {
 	}),
 	))
 	wError.Show()
+	if w != nil {
+		w[0].Close()
+	}
 	wError.SetOnClosed(func() {
 		a.Quit()
 	},
@@ -213,4 +309,55 @@ func windowSize(part float32) fyne.Size {
 		return fyne.NewSize(float32(bounds.Dx())*part, float32(bounds.Dy())*part)
 	}
 	return fyne.NewSize(800, 800)
+}
+
+func getPosicion(a interface{}, c *fyne.Container) int {
+	for i, obj := range c.Objects {
+		if obj == a {
+			return i
+		}
+	}
+	return -1
+}
+
+func ambar(ambar, apagado, sem *canvas.Image, c *fyne.Container) {
+	pos := getPosicion(sem, c)
+
+	for i := 0; i < 10; i++ {
+		c.Objects[pos] = ambar
+		c.Objects[pos].Refresh()
+		time.Sleep(time.Second)
+		c.Objects[pos] = apagado
+		c.Objects[pos].Refresh()
+		time.Sleep(time.Second)
+	}
+}
+
+func colocarBotones(c *fyne.Container, botones []*widget.Button, numDir int, inicializado, editar bool) {
+	if !inicializado {
+		for i := 0; i < numDir; i++ {
+			boton := widget.NewButton("Editar "+fmt.Sprint(i+1), func() {
+			})
+			boton.Hidden = true
+			botones = append(botones, boton)
+			c.Add(boton)
+			switch i {
+			case 0:
+				boton.Move(fyne.NewPos(100, 300))
+			case 1:
+				boton.Move(fyne.NewPos(300, 100))
+			case 2:
+				boton.Move(fyne.NewPos(100, 100))
+			case 3:
+				boton.Move(fyne.NewPos(300, 300))
+			}
+		}
+	} else {
+		fmt.Println("mostrando:", editar)
+
+		for _, boton := range botones {
+			boton.Hidden = editar
+			boton.Refresh()
+		}
+	}
 }
